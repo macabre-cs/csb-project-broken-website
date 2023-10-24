e6b57e0a177abcb5de7b72bfad3d6254b1cde83d
Lotta-Linnea Vahter <macabre.cs@gmail.com>
2023-10-24 18:35:32 +0300

    jeccu oli nyt on basic stuff

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include
 from django.urls import path, re_path
-from users.views import dashboard
+from users.views import dashboard, register
 
 urlpatterns = [
     re_path(r"^dashboard/", dashboard, name="dashboard"),
     re_path(r"^accounts/", include("django.contrib.auth.urls")),
+    re_path(r"^register/", register, name="register"),
+    path("", dashboard, name="home"),
 ]
